72905e34d9ddc4e2389eaa1d314145b5f6425168
duanxinyi3 <duanxinyi3@jd.com>
2021-01-20 15:09:29 +0800

    build命令与loader使用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import './styles.css';
+import Icon from './img.jpg';
+
 function component() {
   let ele = document.createElement('div');
   ele.innerHTML = 'Hello,this is my webpack!';
+  ele.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  ele.appendChild(myIcon);
 
   return ele;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [    // loader 引入载入
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      }
+    ]
   }
 }
