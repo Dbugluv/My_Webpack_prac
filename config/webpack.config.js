a47141b78f1276885e4f3fd5f58a9abc92bcf401
duanxinyi3 <duanxinyi3@jd.com>
2021-02-02 17:47:57 +0800

    项目结构更改

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,10 +5,10 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.js',  // 2020/2/2 Q：此路径是以那处作相对路径处理的？
   output: {
     filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -17,7 +17,7 @@ module.exports = {
       template: 'index.html', //html模板
       inject: true, // true：默认值，script标签位于html文件的 body 底部
     }),
-    new webpack.HotModuleReplacementPlugin(), // 似乎加不加影响不大？
+    new webpack.HotModuleReplacementPlugin(), // 似乎加不加影响不大？ A：hot为 true时，webpack默认加载此插件。
   ],
   module: {
     rules: [    // loader 引入载入
@@ -46,12 +46,15 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: './dist',  // 告知 dev server，从什么位置查找文件
+    contentBase: '../dist',  // 告知 dev server，从什么位置查找文件
     port: 8080,
     hot: true
   },
   resolve: {
     extensions: ['.js', '.json'],   // 解析文件时可接受的扩展名
+    alias: {
+      'SRC': path.join(__dirname, '..', "src")
+    }
   },
   devtool: 'inline-source-map', // 可定位出代码错误位置
 }
